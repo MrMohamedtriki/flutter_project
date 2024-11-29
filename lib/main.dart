c79f414808012943098edfc29ceb33747e5ba2f6
Mohamed Triki <123988516+MrMohamedtriki@users.noreply.github.com>
2024-11-29 20:23:19 +0100

    add Firebase integration and configure plugins for multiple platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/firebase_options.dart';
 import 'package:flutter_project/responsive/mobile.dart';
 import 'package:flutter_project/responsive/responsive.dart';
 import 'package:flutter_project/responsive/web.dart';
 import 'package:flutter_project/screens/register.dart';
 import 'package:flutter_project/screens/sign_in.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+ void main() async { 
+    WidgetsFlutterBinding.ensureInitialized();
+    if (kIsWeb) {
+      await Firebase.initializeApp(
+         options: const FirebaseOptions(
+           apiKey: "dddd",
+           authDomain: "dddd",
+           projectId: "dddd",
+           storageBucket: "dddd",
+           messagingSenderId: "dddd",
+           appId: "dddd"));
+  } else {
+ await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,  
+);
+  }
+ runApp(const MyApp());
+ }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
